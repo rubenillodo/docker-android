b665342db18ad0ee9513b526a98692dae938db4e
Rubén Illodo Brea <me@rubenillodo.com>
2017-11-21 17:52:07 +0800

    Building just the 3 lastest API levels and build tools

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const fs = require('fs-extra');
-const apiLevels = require('./api-levels.json');
-const buildToolsVersions = require('./build-tools-versions.json');
+const fs = require("fs-extra");
+const apiLevels = require("./api-levels.json");
+const buildToolsVersions = require("./build-tools-versions.json");
 
 (async () => {
-  const template = await fs.readFile('./Dockerfile.template', { encoding: 'utf-8' });
+  const template = await fs.readFile("./Dockerfile.template", { encoding: "utf-8" });
 
-  await fs.remove('./build');
+  await fs.remove("./build");
 
-  apiLevels.forEach(async (apiLevel) => {
-    const buildToolsVersion = buildToolsVersions[0];
-    const path = `./build/${apiLevel}/Dockerfile`;
-    const generatedDockerfile = template.replace(/{{API_LEVEL}}/i, apiLevel)
+  await Promise.all(
+    apiLevels.map(async apiLevel => {
+      const buildToolsVersion = buildToolsVersions.find(it => it.includes(apiLevel));
+      const path = `./build/${apiLevel}/Dockerfile`;
+      const generatedDockerfile = template
+        .replace(/{{API_LEVEL}}/i, apiLevel)
         .replace(/{{BUILD_TOOLS_VERSION}}/i, buildToolsVersion);
 
-    await fs.ensureFile(path);
-    await fs.writeFile(path, generatedDockerfile, { encoding: 'utf-8' });
-  });
+      await fs.ensureFile(path);
+      return await fs.writeFile(path, generatedDockerfile, { encoding: "utf-8" });
+    }),
+  );
 })();
